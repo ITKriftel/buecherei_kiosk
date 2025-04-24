6801a3a1834488f5a5a67ace3a353a42133c18e1
ITKriftel <102357229+ITKriftel@users.noreply.github.com>
2025-04-24 13:12:35 +0200

    Add files via upload

---
+++ tweaks.sh
@@ -0,0 +1,15 @@
+# Add Tweaks here
+
+# Autostart Firefox
+sudo mkdir -p "/home/kribu/.config/autostart"
+
+cat > "/home/kribu/.config/autostart/firefox.desktop" <<EOL
+[Desktop Entry]
+Type=Application
+Exec=firefox
+Hidden=false
+NoDisplay=false
+X-GNOME-Autostart-enabled=true
+Name=Firefox
+Comment=Startet Firefox beim Anmelden
+EOL
