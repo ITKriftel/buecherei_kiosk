1494ed3743772fde55616238058cd7d4119e2fba
ITKriftel <102357229+ITKriftel@users.noreply.github.com>
2025-04-24 10:43:22 +0200

    Update tweaks.sh

--- tweaks.sh
+++ tweaks.sh
@@ -1,1 +1,13 @@
 # Add Tweaks here
+
+# Autostart Firefox
+sudo tee ~/.config/autostart/firefox.desktop << EOF > /dev/null
+[Desktop Entry]
+Type=Application
+Exec=firefox
+Hidden=false
+NoDisplay=false
+X-GNOME-Autostart-enabled=true
+Name=Firefox
+Comment=Startet Firefox beim Anmelden
+EOF
